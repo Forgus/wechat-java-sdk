0e7d24e562c326a6776c75417cf7dc5ced606fda
Forgus <chenwb@pingpongx.com>
2018-09-18 18:05:16 +0800

    refactor httpClientUtil

--- src/main/java/com/forgus/wechat/sdk/BaseWechatApi.java
+++ src/main/java/com/forgus/wechat/sdk/BaseWechatApi.java
@@ -32,13 +32,13 @@ public class BaseWechatApi {
     }
 
     public static <T> T get(String url, Class<T> clazz) throws Exception {
-        String result = HttpClientUtil.get(url, null, Consts.UTF_8);
+        String result = HttpClientUtil.get(url);
         logger.info("get request with response:{}", result);
         return JSON.parseObject(result, clazz);
     }
 
     public static <T> T post(String url, String postContext, Class<T> clazz) throws Exception {
-        String result = HttpClientUtil.post(url, postContext, null, Consts.UTF_8);
+        String result = HttpClientUtil.post(url, postContext);
         logger.info("post request with params:{}", postContext);
         return JSON.parseObject(result, clazz);
     }
--- src/main/java/com/forgus/wechat/util/HttpClientUtil.java
+++ src/main/java/com/forgus/wechat/util/HttpClientUtil.java
@@ -2,45 +2,19 @@ package com.forgus.wechat.util;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpHost;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.client.params.ClientPNames;
-import org.apache.http.client.params.CookiePolicy;
 import org.apache.http.conn.ConnectTimeoutException;
-import org.apache.http.conn.routing.HttpRoute;
-import org.apache.http.conn.scheme.PlainSocketFactory;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DecompressingHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.PoolingClientConnectionManager;
-import org.apache.http.impl.conn.SchemeRegistryFactory;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.CoreConnectionPNames;
-import org.apache.http.params.CoreProtocolPNames;
-import org.apache.http.params.HttpParams;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -57,115 +31,23 @@ public class HttpClientUtil {
 
     public static final Charset UTF8 = Charset.forName("UTF-8");
     public static final Charset GB18030 = Charset.forName("GB18030");
-    public static final int TIME_OUT = Integer.getInteger("http.timeout", 60000);
-
-    private static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36";
 
 
-    public static HttpClient HTTP_CLIENT = bulidHttpClient();
-
-    private static HttpClient bulidHttpClient() {
-
-        SchemeRegistry schemeRegistry = SchemeRegistryFactory.createDefault();
-
-        X509TrustManager tm = new X509TrustManager() {
-            public void checkClientTrusted(X509Certificate[] xcs, String string) {
-            }
-
-            public void checkServerTrusted(X509Certificate[] xcs, String string) {
-            }
-
-            public X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
-        };
-        try {
-            SSLContext sslcontext = SSLContext.getInstance("TLS");
-            sslcontext.init(null, new TrustManager[]{tm}, null);
-            SSLSocketFactory socketFactory = new SSLSocketFactory(sslcontext, SSLSocketFactory.STRICT_HOSTNAME_VERIFIER);
-            socketFactory.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-            Scheme sch = new Scheme("https", 443, socketFactory);
-            schemeRegistry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
-            schemeRegistry.register(sch);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (KeyManagementException e) {
-            e.printStackTrace();
-        }
-
-
-        PoolingClientConnectionManager cm = new PoolingClientConnectionManager(schemeRegistry);
-        cm.setMaxTotal(800);
-        cm.setDefaultMaxPerRoute(200);
-
-        cm.setMaxPerRoute(new HttpRoute(new HttpHost("localhost")), 500);
-        cm.setMaxPerRoute(new HttpRoute(new HttpHost("127.0.0.1")), 500);
-        HttpParams defaultParams = new BasicHttpParams();
-
-        defaultParams.setLongParameter(ClientPNames.CONN_MANAGER_TIMEOUT, TIME_OUT);
-        defaultParams.setIntParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, TIME_OUT);//连接超时
-        defaultParams.setIntParameter(CoreConnectionPNames.SO_TIMEOUT, TIME_OUT);//读取超时
-
-        defaultParams.setParameter(ClientPNames.COOKIE_POLICY, CookiePolicy.IGNORE_COOKIES);
-        defaultParams.setParameter(CoreProtocolPNames.HTTP_CONTENT_CHARSET, UTF8.name());
-        defaultParams.setParameter(CoreProtocolPNames.USER_AGENT, USER_AGENT);
-
-
-        HttpClient client = new DefaultHttpClient(cm, defaultParams);
-        client = new DecompressingHttpClient(client);
-        return client;
-    }
+    public static HttpClient HTTP_CLIENT = HttpClientBuilder.create().build();
 
 
     public static String get(String url) throws IOException {
-        return get(url, null, null);
-    }
-
-    public static String get(String url, Map<String, String> headers) throws IOException {
-        return get(url, headers, null);
-    }
-
-    public static String get(String url, Map<String, String> headers, final Charset forceCharset) throws IOException {
         HttpGet get = new HttpGet(url);
-        return execute(get, headers, forceCharset);
+        return execute(get, null, UTF8);
     }
 
 
-
-    public static String post(String url, Map<String, String> params) throws IOException {
-        return post(url,params,null,Consts.UTF_8);
-    }
-
-    public static String post(String url, Map<String, String> params, Map<String, String> headers) throws IOException {
-        return post(url,params,headers,Consts.UTF_8);
-    }
-
-    public static String post(String url, String body, Map<String, String> headers) throws IOException {
+    public static String post(String url, String body) throws IOException {
         HttpPost post = new HttpPost(url);
         if (StringUtils.isNotBlank(body)) {
             post.setEntity(new StringEntity(body, Consts.UTF_8));
         }
-        return execute(post, headers, null);
-    }
-
-    public static String post(String url, Map<String, String> params, Map<String, String> headers, Charset charset) throws IOException {
-        HttpPost post = new HttpPost(url);
-        if (params != null && !params.isEmpty()) {
-            List<NameValuePair> ps = new ArrayList<NameValuePair>(params.size());
-            for (Map.Entry<String, String> kv : params.entrySet()) {
-                ps.add(new BasicNameValuePair(kv.getKey(), kv.getValue()));
-            }
-            post.setEntity(new UrlEncodedFormEntity(ps, Consts.UTF_8));
-        }
-        return execute(post, headers, charset);
-    }
-
-    public static String post(String url, String body, Map<String, String> headers, Charset charset) throws IOException {
-        HttpPost post = new HttpPost(url);
-        if (StringUtils.isNotBlank(body)) {
-            post.setEntity(new StringEntity(body, Consts.UTF_8));
-        }
-        return execute(post, headers, charset);
+        return execute(post, null, UTF8);
     }
 
     private static String execute(final HttpRequestBase request, Map<String, String> headers, final Charset forceCharset) throws IOException {
@@ -204,7 +86,7 @@ public class HttpClientUtil {
 
 
     public static <T> T http(HttpClient client, HttpRequestBase request, Map<String, String> headers, HttpEntityHandler<T> handler)
-            throws IOException {
+        throws IOException {
         if (headers != null && !headers.isEmpty()) {
             for (Map.Entry<String, String> kv : headers.entrySet()) {
                 request.addHeader(kv.getKey(), kv.getValue());
@@ -219,16 +101,12 @@ public class HttpClientUtil {
             client.getConnectionManager().closeIdleConnections(30, TimeUnit.SECONDS);
             throw e;
         } finally {
-            logger.info(handler.getName()+" "+request.getURI()+" ,cost:"+(System.currentTimeMillis() - begin)+"ms");
+            logger.info(handler.getName() + " " + request.getURI() + " ,cost:" + (System.currentTimeMillis() - begin) + "ms");
         }
     }
 
     /**
      * 字符编码检查
-     *
-     * @param html
-     * @param use
-     * @return
      */
     private static Charset checkMetaCharset(String html, Charset use) {
         String magic = "charset=";
